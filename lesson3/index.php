3927531bd7f6685a3c2c41366aac078d376c415e
Jeckvorobey <jeckvorobey69@gmail.com>
2019-04-03 20:50:01 +0300

    выполнил 2 задание

--- lesson3/index.php
+++ lesson3/index.php
@@ -6,12 +6,30 @@ $i = 1; //Счетчик
 $result; //переменная куда заносим результат выполнения условия
 while ($i <= 100) {
     if ($i % 3 === 0) {
-        $result .= $i ." ";
+        $result .= $i . " ";
     }
     $i++;
 }
-echo "<p>" .$result ."</p>";
+echo "<p>" . $result . "</p>";
 
 //задание 2
 echo "<h4> Task 2 </h4>";
 
+function number()
+{
+    $i = 0;
+    $result;
+    do {
+        if ($i === 0) {
+           $result .= $i . " - Это ноль.<br>";
+        } elseif ($i % 2 !== 0) {
+            $result .= $i ." - Это нечётное число.<br>";
+        } else {
+            $result .= $i ." - Это чётное число.<br>";
+        }
+        $i++;
+    }
+     while ($i <= 10);
+     return $result;
+}
+echo number();
