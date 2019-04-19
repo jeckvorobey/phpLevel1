0558c51ef6d3e1d448f46f9c55907c6238e96d2e
jeckvorobey <jeckvorobey69@gmail.com>
2019-04-19 19:17:28 +0300

    реализовал скрипт  вывода каталога

--- lesson6/shop/blocks/addkatalog.php
+++ lesson6/shop/blocks/addkatalog.php
@@ -5,6 +5,15 @@ $sql = 'SELECT * FROM `goods`';
 $result = mysqli_query($connection, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    $row = mysqli_fetch_assoc($result);
-    print_r($row);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $way = 'img/products/'.$row['imgName']; //путь к картинке
+        $head = $row['author'].': '.$row['title']; //заголовок продукта
+        $href = 'blocks/product.php?id='.$row['id']; //путь к скрипту товара
+        echo '<div class="product">';
+        echo '<h4>'.$head.'</h4>';
+        echo '<img src="'.$way.'" alt="'.$row['title'].'" title="'.$head.'">';
+        echo '<p> Цена: '.$row['price'].' руб.</p>';
+        echo '<a href="'.$href.'" target="blank">Подробне...</a>';
+        echo '</div>';
+    }
 }
--- lesson6/shop/katalog.php
+++ lesson6/shop/katalog.php
@@ -25,52 +25,6 @@
         </h1>
         <div id="product-catalog">
             <?=include 'blocks/addkatalog.php'; ?>
-
-          <!--  <div class="product">
-                <h4>
-                    Федор Достоевский: Идиот
-                </h4>
-                <img src="../ShopBooks/img/products/idiot.png" alt="Идиот" title="Федор Достоевский: Идиот">
-                <p>Цена: 100 руб.</p>
-
-                <a href="../ShopBooks/products/idiot.html">Подробнее...</a>
-            </div>
-            <div class="product">
-                <h4>
-                    Наталья Жарова: Невеста по приказу
-                </h4>
-                <img src="../ShopBooks/img/products/NevestaPoPrikazu.png" alt="Невеста по приказу" title="Наталья Жарова: Невеста по приказу">
-                <p>Цена: 150 руб.</p>
-
-                <a href="../ShopBooks/products/NevestaPoPrikazu.html">Подробнее...</a>
-            </div>
-            <div class="product">
-                <h4>
-                    Джек Лондон: Сердца трех
-                </h4>
-                <img src="../ShopBooks/img/products/serdcatreh.jpg" alt="Сердца трех" title="Джек Лондон: Сердца трех">
-                <p>Цена: 100 руб.</p>
-
-                <a href="../ShopBooks/products/serdcatreh.html">Подробнее...</a>
-            </div>
-            <div class="product">
-                <h4>
-                    ФМэт Фицджеральд: Как сильно ты этого хочешь?
-                </h4>
-                <img src="../ShopBooks/img/products/KakSilnoTiEtogoHochesh.png" alt="Как сильно ты этого хочешь?" title="Мэт Фицджеральд: Как сильно ты этого хочешь?">
-                <p>Цена: 200 руб.</p>
-
-                <a href="../ShopBooks/products/KakSilnoTiEtogoHochesh.html">Подробнее...</a>
-            </div>
-            <div class="product">
-                <h4>
-                    Мелинда Солсбери Дочь Пожирательницы Грехов
-                </h4>
-                <img src="../ShopBooks/img/products/DochPosgiratelniciGrehov.jpg" alt="Дочь Пожирательницы Грехов" title="Мелинда Солсбери: Дочь Пожирательницы Грехов">
-                <p>Цена: 200 руб.</p>
-                <a href="../ShopBooks/products/DochPosgiratelniciGrehov.html">Подробнее...</a>
-            </div> -->
-
         </div>
         <footer>
             <p>
