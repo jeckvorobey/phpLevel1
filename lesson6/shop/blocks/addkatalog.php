538ac39c97bab7c6464378a741d9d613e6acccfb
jeckvorobey <jeckvorobey69@gmail.com>
2019-04-19 17:20:14 +0300

    начал писать скрипт формирования каталога

---
+++ lesson6/shop/blocks/addkatalog.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+header('Content-Type: text/html; charset=utf-8');
+include 'blocks/sqlconnect.php';
+$sql = 'SELECT * FROM `goods`';
+$result = mysqli_query($connection, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    print_r($row);
+}
