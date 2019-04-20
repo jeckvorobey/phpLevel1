b4240ab09ec85e2dedd905a5d41f2d5751debc1e
jeckvorobey <jeckvorobey69@gmail.com>
2019-04-20 11:44:54 +0300

    Исправил недочеты, добавил значения по умолчанию

--- lesson6/index.php
+++ lesson6/index.php
@@ -9,19 +9,29 @@
 </head>
 <body>
 <?php
-if (isset($_GET['btn'])) {
+
+ if (isset($_GET['num1'])) {
     $num1 = (isset($_GET['num1'])) ? (int) $_GET['num1'] : 0;
+    } else $num1 = 1;
+if(isset($_GET['num2'])){
     $num2 = (isset($_GET['num2'])) ? (int) $_GET['num2'] : 0;
+    } else $num2 =1; 
+if(isset($_GET['operation'])){
     $operation = $_GET['operation'];
-    $res = mathOperation($num1, $num2, $operation);
-}
+}else $operation = '+';
+$res = mathOperation($num1, $num2, $operation);
+
 //калькулятор задание 2
-if (isset($_POST['operation'])) {
+if(isset($_POST['number1'])) {
     $number1 = (isset($_POST['number1'])) ? (int) $_POST['number1'] : 0;
+}else $number1 = 1;
+if(isset($_POST['number2'])) {
     $number2 = (isset($_POST['number2'])) ? (int) $_POST['number2'] : 0;
-    $op = $_POST['operation'];
-    $result = mathOperation($number1, $number2, $op);
-}
+} else $number2 = 1;
+
+$op = $_POST['operation'];
+$result = mathOperation($number1, $number2, $op);
+
 //функция калькуляции
 function mathOperation($num1, $num2, $operation)
 {
@@ -51,7 +61,7 @@ function mathOperation($num1, $num2, $operation)
 ?>
     <p>Калькулятор задание 1</p>
     <form action="#" method="get">
-        <input class="num" type="text" name="num1" value="<?=$_GET['num1']; ?>">
+        <input class="num" type="text" name="num1" value="<?=$num1 ?>">
         <select name="operation" required>
             <option value="operation">Выберите операцию</option>
             <option value="+">+</option>
@@ -59,15 +69,15 @@ function mathOperation($num1, $num2, $operation)
             <option value="*">*</option>
             <option value="/">/</option>
         </select>
-        <input class="num" type="text" name="num2" value="<?=$_GET['num2']; ?>">
+        <input class="num" type="text" name="num2" value="<?=$num2 ?>">
         <input type="submit" name='btn' value="=">
         <span><?=$res; ?> </span>
     </form>
     <hr>
     <p>Калькулятор задание 2</p>
     <form action="#" method="post">
-        <input class="num" type="text" name="number1" value="<?=$_POST['number1']; ?>">
-        <input class="num" type="text" name="number2" value="<?=$_POST['number2']; ?>">
+        <input class="num" type="text" name="number1" value="<?=$number1 ?>">
+        <input class="num" type="text" name="number2" value="<?=$number2 ?>">
         <span>=</span>
         <span><?=$result; ?></span>
         <br>
