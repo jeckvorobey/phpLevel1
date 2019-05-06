49c5b937405d3c123a791292238d2b35f7f39723
jeckvorobey <jeckvorobey69@gmail.com>
2019-05-06 13:31:54 +0300

    написал добавление и увеличение кол-ва в корзине

--- lesson7/shop/controllers/basket.php
+++ lesson7/shop/controllers/basket.php
@@ -5,22 +5,23 @@ include_once '../blocks/db_request.php';
 
 if (isset($_POST['id'])) {
     $goodId = (int)$_POST['id'];
-
+//получаем id user если авторизован и id его ссессии если не авторизован
     if(isset($_SESSION['login']) && isset($_SESSION['pass'])) {
         $userId = $_SESSION['id'];
     }else $userId = session_id();
-    $good = goods_get($connection, $id);
-    $goodRow = get_basket_row($connection, $userId, $goodId);
-    if(mysqli_num_rows($goodRow) > 0){
-        $upCount = up_date_count($connection, $userId, $goodId);
-        echo $data = json_encode($goodRow['count']);
+
+    $good = goods_get($connection, $id); //получаем товар по id
+    $goodRow = get_basket_row($connection, $userId, $goodId);//строка из карзины с id user и id good и количество в корзине
+    if(!$goodRow){
+        $addGood = add_basket($connection, $userId, $goodId); //если товара в корзине нет добавляем
+        // echo $data = json_encode('успешно добавлен');
     } else {
-        $addGood = add_basket($connection, $userId, $goodId);
-        echo $data = json_encode($good['author']. ' : ' .$good['title']. 'успешно добавлен');
-    
+        $count = (int)$goodRow['count'];
+        $count++;
+        $upCount = up_date_count($connection, $userId, $goodId, $count); //увеличиваем количество на 1
+        // echo $data = json_encode($count);
     }
 
-    
     // echo $data = json_encode($userId);
 };
 
--- lesson7/shop/models/modelBasket.php
+++ lesson7/shop/models/modelBasket.php
@@ -1,7 +1,7 @@
 <?php 
 
 require_once '../blocks/sqlconnect.php';
-
+//получаем строку с товаром из корзины (если такая есть)
 function get_basket_row ($connection, $userId, $goodId) 
 {
     $idGood = (int)$goodId;
@@ -13,14 +13,17 @@ function get_basket_row ($connection, $userId, $goodId)
         die(mysqli_error($connection));
     }
 
-    return $result;
-}
+    $good = mysqli_fetch_assoc($result);
 
-function up_date_count ($connection, $userId, $goodId) 
+    return $good;
+}
+//обновляем количество в корзине
+function up_date_count ($connection, $userId, $goodId, $count) 
 {
     $idGood = (int)$goodId;
-    $sql = "UPDATE `basket` SET `count`='count++' WHERE `id_good`='%d' and `id_user`='%s'";
-    $query = sprintf($sql, $idGood, mysqli_escape_string($connection, $userId));
+    $counter = (int)$count;
+    $sql = "UPDATE `basket` SET `count`='%d' WHERE `id_good`='%d' and `id_user`='%s'";
+    $query = sprintf($sql, $counter, $idGood, mysqli_escape_string($connection, $userId));
     $result = mysqli_query($connection, $query);
 
     if (!$result) {
@@ -29,7 +32,7 @@ function up_date_count ($connection, $userId, $goodId)
 
     return mysqli_affected_rows($connection);
 }
-
+//добавляем товар в корзину
 function add_basket ($connection, $userId, $goodId)
 {
     $idGood = (int)$goodId;
